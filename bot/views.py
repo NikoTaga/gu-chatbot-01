4f5473b25e08e984b595ace2481b01aabd47323f
Vlad Chekunov <gwellir@gmail.com>
2020-11-09 15:18:00 +0300

    fix word wrapping

--- bot/views.py
+++ bot/views.py
@@ -28,7 +28,7 @@ def chat_list(request: HttpRequest) -> HttpResponse:
     chats: List[Dict[str, Any]] = [
         {
             'number': i,
-            'name': f'chat number #{i}',
+            'name': f'chat number {"f"*100} #{i}',
             'time': datetime.now().date()
         } for i in range(20)
     ]
@@ -44,14 +44,14 @@ def chat_view(request: HttpRequest, pk: int) -> HttpResponse:
     chats: List[Dict[str, Any]] = [
         {
             'number': i,
-            'name': f'chat number #{i}',
+            'name': f'chat numberf  {"f"*100}  #{i}',
             'time': datetime.now().date()
          } for i in range(20)
     ]
     messages: List[Dict[str, Any]] = [
         {
             'number': j,
-            'content': f'Test message {pk}#{j}',
+            'content': f'Test message {"f"*100} {pk}#{j}',
             'direction': bool(j % 2),
             'time': datetime.now().strftime('%H:%M %d-%m-%Y'),
         } for j in range(20)
