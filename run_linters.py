572ad5ef772eaedf1a1ed5163eb18bded151b10d
ss2576 <ss2576@mail.ru>
2020-11-11 09:06:17 +0300

    исправлены ошибки

--- run_linters.py
+++ run_linters.py
@@ -6,11 +6,14 @@ import sys
 
 PATH = os.getcwd()
 DIR_REPORTS = 'report_linters'
+DIRS = ['./billing', './bot', './clients', './ecom_chatbot', './shop', './']
+# формирует список папок и файлов для проверки #
+# DIRS = os.listdir(PATH)
 
 if platform.system() == 'Windows':
-    PATH_LINT = f'{PATH}\{DIR_REPORTS}'
+    PATH_LINT = r'%s\%s' % (PATH, DIR_REPORTS)
 elif platform.system() in ['Linux', 'Darwin']:
-    PATH_LINT = f'{PATH}/{DIR_REPORTS}'
+    PATH_LINT = r'%s/%s' % (PATH, DIR_REPORTS)
 else:
     print('Unknown OS!\nExiting')
     sys.exit(1)
@@ -21,13 +24,23 @@ mode_type = str(input('Для интерактивного режима введ
 if not os.path.exists(PATH_LINT):
     os.mkdir(PATH_LINT, access_rights)
 
+
+
 files_in_directory = glob.glob(os.path.join(PATH_LINT, '*.*'))
 for file in files_in_directory:
     os.remove(file)
 
 if mode_type == 'Y':
-    subprocess.run(['mypy',	'./billing', './bot', './clients', './ecom_chatbot', './shop', './'])
+    print(f'Проверка линтером MYPY: \n')
+    for directory in DIRS:
+        print(f'Проверяем папку {directory}:')
+        subprocess.run(['mypy', directory])
+    print(f'Проверка линтером FLAKE8: \n')
     subprocess.run(['flake8', './'])
 else:
-    subprocess.run(['mypy', './billing', './bot', './clients', './ecom_chatbot', './shop', './'])
+    print(f'Проверка линтером MYPY: \n')
+    for directory in DIRS:
+        print(f'Проверяем папку {directory}:')
+        subprocess.run(['mypy', directory])
+    print(f'Проверка линтером FLAKE8: \n')
     subprocess.run(['flake8', '--output-file=report_linters/flake8.log', './'])
