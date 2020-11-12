b84d66f2b75d779c245e7fe661894385155ed2bf
NiShadrin <nishadrin@gmail.com>
2020-11-12 17:30:46 +0300

    create variable developers for index page context and fix in index.html

--- shop/views.py
+++ shop/views.py
@@ -2,9 +2,18 @@
 from django.shortcuts import render
 
 
+developers = (
+    'Шадрин Николай Николаевич',
+    'Шредер Юрий Вальтрович',
+    'Сергеев Сергей Александрович',
+    'Мельников Александр Валерьевич',
+    'Чекунов Владислав Юрьевич',
+    )
+
 def index_page(request) -> render:
     context = {
-        "title_page": 'Основная страница',
-    }
-    
+        'title_page': 'Основная страница',
+        'developers': developers,
+        }
+
     return render(request, 'shop/index.html', context)
