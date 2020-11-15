00721ad2839d867c2359e5f994db638fb8f0edc1
Vlad Chekunov <gwellir@gmail.com>
2020-11-15 22:40:31 +0300

    add jivosite structure

---
+++ clients/jivo_constants.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class ResponseType:
+    OK = '200 OK'
+    BAD_REQUEST = '400 Bad Request'
+    UNAUTHORIZED = '401 Unauthorized'
+    FORBIDDEN = '403 Forbidden'
+    NOT_FOUND = '404 Not Found'
+    METHOD_NOT_ALLOWED = '405 Method Not Allowed'
+    TOO_MANY_REQUESTS = '429 Too Many Request'
+    INTERNAL_SERVER_ERROR = '500 Internal Server Error'
+    BAD_GATEWAY = '502 Bad Gateway'
+    SERVICE_UNAVAILABLE = '503 Service Unavailable'
+    GATEWAY_TIMEOUT = '504 Gateway Timeout'
+
+
+class JivoEventType(Enum):
+    CLIENT_MESSAGE = 'CLIENT_MESSAGE'
+    BOT_MESSAGE = 'BOT_MESSAGE'
+    INVITE_AGENT = 'INVITE_AGENT'
+    AGENT_JOINED = 'AGENT_JOINED'
+    AGENT_UNAVAILABLE = 'AGENT_UNAVAILABLE'
+
+
+class JivoMessageType(Enum):
+    TEXT = 'TEXT'
+    MARKDOWN = 'MARKDOWN'
+    BUTTONS = 'BUTTONS'
---
+++ clients/jivo_entities.py
@@ -0,0 +1,77 @@
+from dataclasses import field
+from typing import ClassVar, List, Optional, Type
+
+import marshmallow
+import marshmallow_enum
+from marshmallow_dataclass import dataclass
+
+from .jivo_constants import *
+from .ok_entities import SkipNoneSchema
+
+
+@dataclass(order=True)
+class Error:
+    Schema: ClassVar[Type[marshmallow.Schema]] = marshmallow.Schema
+
+    code: ResponseType = field(
+        metadata={
+            "marshmallow_field": marshmallow_enum.EnumField(ResponseType, by_value=True)
+        }
+    )
+    message: Optional[str]
+
+
+@dataclass(order=True, base_schema=SkipNoneSchema)
+class ErrorMessage:
+    Schema: ClassVar[Type[marshmallow.Schema]] = marshmallow.Schema
+
+    error: Error
+
+
+@dataclass(order=True)
+class JivoButton:
+    Schema: ClassVar[Type[marshmallow.Schema]] = marshmallow.Schema
+
+    text: str
+    id: str
+
+
+@dataclass(order=True, base_schema=SkipNoneSchema)
+class JivoMessage:
+    Schema: ClassVar[Type[marshmallow.Schema]] = marshmallow.Schema
+
+    type: JivoMessageType = field(
+        metadata={
+            "marshmallow_field": marshmallow_enum.EnumField(JivoMessageType, by_value=True)
+        }
+    )
+    text: str
+    timestamp: int
+    button_id: Optional[int]
+    title: Optional[str]
+    content: Optional[str]
+    buttons: Optional[List[JivoButton]] = field(
+        default=None,
+        metadata={
+            "marshmallow_field": marshmallow.fields.List(
+                marshmallow.fields.Nested(JivoButton.Schema()),
+                allow_none=True,
+                validate=marshmallow.validate.Length(max=3),
+            )
+        }
+    )
+
+
+@dataclass(order=True, base_schema=SkipNoneSchema)
+class JivoEvent:
+    Schema: ClassVar[Type[marshmallow.Schema]] = marshmallow.Schema
+
+    event: JivoEventType = field(
+        metadata={
+            "marshmallow_field": marshmallow_enum.EnumField(JivoEventType, by_value=True)
+        }
+    )
+    id: str
+    client_id: str
+    chat_id: Optional[str]
+    message: JivoMessage
