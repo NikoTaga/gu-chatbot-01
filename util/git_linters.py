01549a1a35eb05c17a6fee09b48c905a4bbc95cf
sergeev_sa <ss2576@mail.ru>
2020-12-14 15:19:23 +0300

    lunters 3

--- util/git_linters.py
+++ util/git_linters.py
@@ -1,34 +1,24 @@
 import subprocess
-import platform
-import os
-import glob
 import sys
 
-PATH = os.getcwd()
-DIR_REPORTS = '../report_linters'
-DIRS = ['./billing', './bot', './clients', './ecom_chatbot', './shop', './']
-# формирует список папок и файлов для проверки #
-# DIRS = os.listdir(PATH)
-
-if platform.system() == 'Windows':
-    PATH_LINT = r'%s\%s' % (PATH, DIR_REPORTS)
-elif platform.system() in ['Linux', 'Darwin']:
-    PATH_LINT = r'%s/%s' % (PATH, DIR_REPORTS)
-else:
-    print('Unknown OS!\nExiting')
-    sys.exit(1)
-
-access_rights = 0o777
-
-if not os.path.exists(PATH_LINT):
-    os.mkdir(PATH_LINT, access_rights)
-
-
-files_in_directory = glob.glob(os.path.join(PATH_LINT, '*.*'))
-for file in files_in_directory:
-    os.remove(file)
+DIRS = ['../billing', '../bot', '../clients', '../ecom_chatbot', '../shop']
+# IGNORE_FLAKE = '--extend-ignore=D210'
 
 
 for directory in DIRS:
-    subprocess.run(['mypy', directory])
-subprocess.run(['flake8', './'])
+    result = subprocess.run(['mypy', directory])
+    result_code = result.returncode
+    if result_code == 0:
+        print('Mypy: ошибки оформления кода не найдены.')
+    else:
+        print('Mypy: найдены ошибки оформления кода!!!')
+        sys.exit(1)
+
+
+result = subprocess.run(['flake8', '../'])
+result_code = result.returncode
+if result_code == 0:
+    print('flake8: ошибки оформления кода не найдены.')
+else:
+    print('flake8: найдены ошибки оформления кода!!!')
+    sys.exit(1)
