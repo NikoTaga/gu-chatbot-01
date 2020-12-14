8db9b88648d5c728b122ce20ca0693df193066bc
sergeev_sa <ss2576@mail.ru>
2020-12-14 15:21:55 +0300

    lunters 4

--- util/git_linters.py
+++ util/git_linters.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-DIRS = ['../billing', '../bot', '../clients', '../ecom_chatbot', '../shop']
+DIRS = ['./billing', './bot', './clients', './ecom_chatbot', './shop']
 # IGNORE_FLAKE = '--extend-ignore=D210'
 
 
@@ -15,7 +15,7 @@ for directory in DIRS:
         sys.exit(1)
 
 
-result = subprocess.run(['flake8', '../'])
+result = subprocess.run(['flake8', './'])
 result_code = result.returncode
 if result_code == 0:
     print('flake8: ошибки оформления кода не найдены.')
