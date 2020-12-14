3b7b0c053b136129351c0505f0a58e9c7258cf6f
Vlad Chekunov <gwellir@gmail.com>
2020-12-14 16:20:11 +0300

    fix type checking

--- billing/managers.py
+++ billing/managers.py
@@ -7,7 +7,7 @@ from django.db.models.query import QuerySet
 
 from billing.exceptions import UpdateCompletedCheckoutError
 from shop.models import Order
-from common.constants import OrderStatus
+from common.constants import OrderStatus, PaymentSystem
 from billing.constants import PaypalOrderStatus
 
 if TYPE_CHECKING:
@@ -21,7 +21,7 @@ class CheckoutManager(models.Manager):
     """Менеджер управляет выписанными Checkout."""
 
     def make_checkout(self,
-                      payment_system: int,
+                      payment_system: PaymentSystem,
                       tracking_id: Union[str, int],
                       order_id: int,
                       payment_status: Optional[str] = None) -> None:
@@ -29,7 +29,7 @@ class CheckoutManager(models.Manager):
 
         order = Order.objects.get_order(order_id).first()
         Order.objects.update_order(order_id, OrderStatus.PENDING_PAYMENT.value)
-        checkout = self.create(order=order, system=payment_system, tracking_id=tracking_id, status=payment_status)
+        checkout = self.create(order=order, system=payment_system.value, tracking_id=tracking_id, status=payment_status)
         return checkout
 
     def get_checkout(self, checkout_id: Union[str, int]) -> QuerySet:
