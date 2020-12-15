4142e9073038c0b6f274ebe46ccaafe0f8f7b3aa
Vlad Chekunov <gwellir@gmail.com>
2020-12-15 06:32:08 +0300

    add stripe cancel page

--- billing/urls.py
+++ billing/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('stripe_webhook/', billing.stripe_webhook),
     path('stripe_redirect/<str:cid>', billing.stripe_redirect),
     path('stripe_success/<int:order_id>', billing.stripe_payment_success),
-    # path('stripe_cancel/<int:order_id>', billing.stripe_payment_cancel),
+    path('stripe_cancel/<int:order_id>', billing.stripe_payment_cancel),
 ]
